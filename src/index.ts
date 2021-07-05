804a318710ff5969db6f75a0d42fd3a313e01547
NoriX <versatility.95@mail.ru>
2021-07-05 17:15:47 +0300

    Events refactored into separate module using composition

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 import { User } from "./models/User";
 
 const user = new User({ id: 1, name: "SavedName", age: 30 });
-user.save();
+user.events.on("change", () => {
+  console.log("change!");
+});
+
+user.events.trigger("change");
