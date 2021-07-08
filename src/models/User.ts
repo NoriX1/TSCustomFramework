7cd6fda39da5b194c1c2408aea706fb595ece22d
Dmitry <versatility.95@mail.ru>
2021-07-08 20:33:27 +0300

    User refactored with inheritance

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,7 +1,7 @@
-import { Eventing } from "./Eventing";
-import { Sync } from "./Sync";
+import { Model } from "./Model";
 import { Attributes } from "./Attributes";
-import { AxiosResponse } from "axios";
+import { ApiSync } from "./ApiSync";
+import { Eventing } from "./Eventing";
 
 export interface UserProps {
   id?: number;
@@ -11,64 +11,12 @@ export interface UserProps {
 
 const rootUrl = "http://localhost:3000/users";
 
-export class User {
-  public events: Eventing = new Eventing();
-  public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
-  public attributes: Attributes<UserProps>;
-
-  constructor(attrs: UserProps) {
-    this.attributes = new Attributes<UserProps>(attrs);
-  }
-  /* Bad approach. We need to pass through all args.
-   * In case, if we will change args in events.on func., we should find
-   * all use cases and in every case change arguments of functions.
-   * It might be a nightmare :)
-
-    on(eventName: string, callback: Callback): void {
-      this.events.on(eventName, callback);
-    }
-  */
-
-  /* With getters we shouldn't pass any args, because we return direct reference
-   * to the events.on function so, we will escape possible problems of previous variant
-   */
-  get on() {
-    return this.events.on;
-  }
-
-  get trigger() {
-    return this.events.trigger;
-  }
-
-  get get() {
-    return this.attributes.get;
-  }
-
-  set(update: UserProps): void {
-    this.attributes.set(update);
-    this.events.trigger("change");
-  }
-
-  fetch(): void {
-    const id = this.attributes.get("id");
-
-    if (typeof id !== "number") {
-      throw new Error("Cannot fetch without an id");
-    }
-
-    this.sync.fetch(id).then((response: AxiosResponse): void => {
-      this.set(response.data);
-    });
-  }
-
-  save(): void {
-    this.sync
-      .save(this.attributes.getAll())
-      .then((_: AxiosResponse): void => {
-        this.trigger("save");
-      })
-      .catch(() => {
-        this.trigger("error");
-      });
+export class User extends Model<UserProps> {
+  static buildUser(attrs: UserProps): User {
+    return new User(
+      new Attributes<UserProps>(attrs),
+      new Eventing(),
+      new ApiSync<UserProps>(rootUrl)
+    );
   }
 }
