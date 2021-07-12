64d43d7174ab9086ff66f80e18379096812892a8
Dmitry <versatility.95@mail.ru>
2021-07-12 19:49:49 +0300

    CollectionView class. Fetching, Rendering collection of Users.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import { UserEdit } from "./views/UserEdit";
 import { User } from "./models/User";
+import { UserList } from "./views/UserList";
+import { Collection } from "./models/Collection";
+import { UserProps } from "./models/User";
 
 const user = User.buildUser({ name: "User Name", age: 20 });
 
@@ -12,3 +15,20 @@ if (root) {
 } else {
   throw new Error("Root element not found");
 }
+
+const users = new Collection(
+  "http://localhost:3000/users",
+  (json: UserProps) => {
+    return User.buildUser(json);
+  }
+);
+
+users.on("change", () => {
+  const root = document.getElementById("collectionRoot");
+
+  if (root) {
+    new UserList(root, users).render();
+  }
+});
+
+users.fetch();
