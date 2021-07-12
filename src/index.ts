b12ef660af4749ebcf745010489220d0a58a3fd0
Dmitry <versatility.95@mail.ru>
2021-07-12 19:28:49 +0300

    Rendering UserEdit with UserForm and UserShow

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { UserEdit } from "./views/UserEdit";
 import { User } from "./models/User";
 
-const user = User.buildUser({ name: "NAME", age: 20 });
+const user = User.buildUser({ name: "User Name", age: 20 });
 
 const root = document.getElementById("root");
 
@@ -9,7 +9,6 @@ if (root) {
   const userEdit = new UserEdit(root, user);
 
   userEdit.render();
-  console.log(userEdit);
 } else {
   throw new Error("Root element not found");
 }
